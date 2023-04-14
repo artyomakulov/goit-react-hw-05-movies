2e1f67efc9ba0325e886d6843829d63c1971daa5
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2023-04-14 15:05:13 +0300

    Update Movies.jsx

--- src/pages/Movies.jsx
+++ src/pages/Movies.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Movies() {
   const [searchFilm, setSearchFilm] = useState(false);
