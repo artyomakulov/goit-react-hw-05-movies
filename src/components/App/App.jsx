dc6cf7e74b5dbb7a5d5a7826aceba3a07b652096
artyomakulov <88495795+artyomakulov@users.noreply.github.com>
2023-04-18 22:42:59 +0300

    404page

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,10 +7,12 @@ const Movies = lazy(() => import('pages/Movies/Movies'));
 const MovieDetails = lazy(() => import('pages/MovieDetails/MovieDetails'));
 const Cast = lazy(() => import('../Cast/Cast'));
 const Reviews = lazy(() => import('../Reviews'));
+const NotFound = lazy(() => import('pages/NotFound/NotFound'));
 
 export const App = () => {
   return (
     <Routes>
+      <Route path="*" element={<NotFound />} />
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
